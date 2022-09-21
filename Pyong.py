0e43c89161fe22d60d887fa4a7ce5679da9b3810
ryandefea <ryan.defea@gmail.com>
2022-09-21 12:46:18 -0500

    paddle b now moving up and down

--- Pyong.py
+++ Pyong.py
@@ -42,12 +42,24 @@ def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)    
     
 
 # Keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "q")
-wn.onkeypress(paddle_a_down, "a")    
+wn.onkeypress(paddle_a_down, "a")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")    
 
 #main game loop
 while True:
